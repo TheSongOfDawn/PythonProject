c862f6703b25cf42721be6f3549bb6352e602820
lk <405099871@qq.com>
2017-07-31 11:27:55 +0800

    Python 函数的参数

--- reduce.py
+++ reduce.py
@@ -33,3 +33,5 @@ print(list(filter(is_odd,[1,2,3,4,5,6,7,8,9,10,11,12,13,45,15])))
 #sorted 排序算法
 #sorted()函数就可以对list进行排序 它还可以接收一个key函数来实现自定义的排序，例如按绝对值大小排序：
 # y指定的函数将作用于list的每一个元素上，并根据key函数返回的结果进行排序。
+#要进行反向排序，不必改动key函数，可以传入第三个参数reverse=True：
+#sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=True)
