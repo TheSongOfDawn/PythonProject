b0a8ef4145cd62ba76d316b7e0c00b88acbe1465
Osero <405099871@qq.com>
2018-09-29 15:37:51 +0800

    update

--- myPythonMonitor/main.py
+++ myPythonMonitor/main.py
@@ -1,7 +1,7 @@
 # -*- coding: UTF-8 -*-
-from drawLog import *
 from monitor import *
 
 if __name__ == '__main__':
     monitorMain() #统计
-    perfstatMain() #绘图
+    #from drawLog import *
+    #perfstatMain() #绘图
