d3c1f73a70ea9736213d75aeaed724bfba55dc54
Osero <405099871@qq.com>
2018-09-29 17:49:43 +0800

    update

--- myPythonMonitor/main.py
+++ myPythonMonitor/main.py
@@ -1,8 +1,16 @@
 # -*- coding: UTF-8 -*-
 from monitor import *
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 if __name__ == '__main__':
+    print("******Watching:******\n")
     monitorMain() #统计
+    print("******monitorMain() over!!******")
+    from analysis import *
+    ResourceAnalysis() #分析
+    print("******ResourceAnalysis() over!!******")
     #from drawLog import *
     #perfstatMain() #绘图
-    raw_input("press <ENTER>")
